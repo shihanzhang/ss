7673596859cdbf48c21f929223f1af9bc233e7ca
shihanzhang <ayshihanzhang@gmail.com>
2017-04-02 11:00:15 +0800

    Use pbr to manager setup

--- setup.py
+++ setup.py
@@ -1,39 +1,27 @@
-import codecs
-from setuptools import setup
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+# THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
+import setuptools
 
-with codecs.open('README.rst', encoding='utf-8') as f:
-    long_description = f.read()
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing  # noqa
+except ImportError:
+    pass
 
-setup(
-    name="shadowsocks",
-    version="2.6.12",
-    license='http://www.apache.org/licenses/LICENSE-2.0',
-    description="A fast tunnel proxy that help you get through firewalls",
-    author='clowwindy',
-    author_email='clowwindy42@gmail.com',
-    url='https://github.com/shadowsocks/shadowsocks',
-    packages=['shadowsocks', 'shadowsocks.crypto', 'shadowsocks.obfsplugin'],
-    package_data={
-        'shadowsocks': ['README.rst', 'LICENSE']
-    },
-    install_requires=[],
-    entry_points="""
-    [console_scripts]
-    sslocal = shadowsocks.local:main
-    ssserver = shadowsocks.server:main
-    """,
-    classifiers=[
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Internet :: Proxy Servers',
-    ],
-    long_description=long_description,
-)
+setuptools.setup(
+    setup_requires=['pbr>=1.8'],
+    pbr=True)
